3cad9ee58659afc59f7df1a6c06a82355423dc0c
sakiya <ssif8xygm3@gmail.com>
2022-01-07 08:52:11 +0900

    add:DAY2のテキストpp.9を参考にフライトモード番号を追加

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -38,6 +38,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+        ALPHABET =     29,  // アルファベット飛行用のモード(コース3)
     };
 
     // constructor
